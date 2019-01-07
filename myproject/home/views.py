b725f75e4908b9139540ec87955085d99a66f773
Alex Pelaez <pelaezalex8@gmail.com>
2019-01-06 22:37:54 -0700

    updated navbar, created class based home page

--- myproject/home/views.py
+++ myproject/home/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def home(request):
-    return HttpResponse('It works')
+from django.views.generic import TemplateView
+
+class HomeView(TemplateView):
+    template_name = 'home/home.html'
